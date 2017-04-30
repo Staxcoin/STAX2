c8ff0e7ef52272cac87569a29f4fe1286ca8ee75
Staxcoin <staxcoin@staxcoin.net>
2017-04-30 17:27:19 +0200

    Message de validation

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Staxcoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "StaxcoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
